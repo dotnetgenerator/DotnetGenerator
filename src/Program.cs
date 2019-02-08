e9b86420de4a7f8e47247589a4d1ac3ca90e3e85
Michael Prattinger <mprattinger@outlook.com>
2019-02-08 10:07:06 +0100

    Initial

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dgen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
